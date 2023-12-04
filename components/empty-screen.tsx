d31dfc2a0d555f8b8f2d3c5ed531129ffb58d3b8
Saksham Gupta <gupta.saksham98@gmail.com>
2023-12-04 12:51:35 -0500

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,20 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Mr. Potter!',
+    message: `Mr.Potter, What are you doing here?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Its always you three!',
+    message: 'Why is it when something happens, its always you three?'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Owl',
+    message: `I need an Owl!`
+  },
+  {
+    heading: 'Basilisk',
+    message: `The basilisk kills people by looking at them.`
   }
 ]
 
@@ -24,18 +28,14 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to the MistralPotter Chatbot!
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
+          This is an open source chatbot in the style of Harry Potter powered by the Mistral 7B model, built by Saksham Gupta, Amita Jadhav, and Siddhi Jadhav for their cloud computing project.
           .
         </p>
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          You can start a conversation here or try the following magical examples:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
